8e5704b692431de239d6d0daee4d270e59bd4f72
sat-mbp <sat@hongo.wide.ad.jp>
2017-07-05 19:25:26 +0900

    minor fix

--- amulog/config.py
+++ amulog/config.py
@@ -194,7 +194,7 @@ def open_config(fn = None, ex_defaults = None,
         while conf.has_option(IMPORT_SECTION, IMPORT_OPTION):
             import_fn = conf.get(IMPORT_SECTION, IMPORT_OPTION)
             conf.remove_option(IMPORT_SECTION, IMPORT_OPTION)
-            import_conf = config.parser.ConfigParser().read(import_fn)
+            import_conf = configparser.ConfigParser().read(import_fn)
             import_config(conf, import_conf)
 
     if not nodefault:
