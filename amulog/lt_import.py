55044792814d8e5bb15b14795ae9a271249ddea6
sat-mbp <sat@hongo.wide.ad.jp>
2017-11-16 14:54:55 +0900

    fix bug in lt_import.]search_exception

--- amulog/lt_import.py
+++ amulog/lt_import.py
@@ -81,7 +81,7 @@ def search_exception(conf, targets, form):
         for l_w, l_s in logparser.iter_lines(conf, fn, form):
             tid, dummy = ltgen.process_line(l_w, l_s)
             if tid is None:
-                print(line.rstrip("\n"))
+                print(" ".join(l_w))
         _logger.info("lt_import job for ({0}) done".format(fn))
 
 
