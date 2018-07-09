3555428af91a47f77057923c4292872e5de130cd
sat-nii-mbp <sat@nii.ac.jp>
2018-07-09 13:12:58 +0900

    add logparser_test

---
+++ scripts/logparser_test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import sys
+from amulog import logparser
+from amulog import config
+from amulog import common
+
+if len(sys.argv) < 3:
+    sys.exit("usage: {0} config targets".format(sys.argv[0]))
+conf = config.open_config(sys.argv[1])
+
+lp = logparser.LogParser(conf)
+for fp in common.rep_dir(sys.argv[2:]):
+    with open(fp) as f:
+        for line in f:
+            #print LP.process_line(line.rstrip("\n"))
+            print("{0}|{1}|{2}|{3}".format(*lp.process_line(line)))
+
